3d4d371f952285261ba93a40f0d171524ca8c573
kpcyrd <git@rxv.cc>
2024-01-04 22:00:49 +0100

    Add shell completions code

--- src/args.rs
+++ src/args.rs
@@ -1,13 +1,25 @@
+use crate::errors::*;
+use clap::{CommandFactory, Parser};
+use clap_complete::Shell;
+use std::io::stdout;
 use std::path::PathBuf;
 
 /// Flash an AVR firmware from an elf file with micronucleus
-#[derive(Debug, clap::Parser)]
+#[derive(Debug, Clone, Parser)]
 pub struct Args {
     /// The elf file containing the firmware
-    pub input: PathBuf,
+    pub input: Option<PathBuf>,
     /// Instead of flashing, write the firmware to file
     pub output: Option<PathBuf>,
     /// Timeout in seconds for micronucleus device discovery
     #[arg(short, long)]
     pub timeout: Option<u64>,
+    /// Generate shell completions
+    #[arg(long, hide = true)]
+    pub completions: Option<Shell>,
+}
+
+pub fn gen_completions(shell: Shell) -> Result<()> {
+    clap_complete::generate(shell, &mut Args::command(), "elf2nucleus", &mut stdout());
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -20,48 +20,56 @@ fn main() -> Result<()> {
     env_logger::init_from_env(Env::default().default_filter_or(log_level));
     debug!("Started with arguments: {args:?}");
 
-    let output = Command::new("avr-objcopy")
-        .args([
-            OsStr::new("--output-target=ihex"),
-            args.input.as_ref(),
-            OsStr::new("/dev/stdout"),
-        ])
-        .output()
-        .context("Failed to execute avr-objcopy")?;
+    match (&args.input, args.completions) {
+        (Some(input), None) => {
+            let output = Command::new("avr-objcopy")
+                .args([
+                    OsStr::new("--output-target=ihex"),
+                    input.as_ref(),
+                    OsStr::new("/dev/stdout"),
+                ])
+                .output()
+                .context("Failed to execute avr-objcopy")?;
 
-    if !output.status.success() {
-        anyhow::bail!("avr-objcopy exited with error: {:?}", output.status);
-    }
+            if !output.status.success() {
+                anyhow::bail!("avr-objcopy exited with error: {:?}", output.status);
+            }
 
-    let hex = &output.stdout;
+            let hex = &output.stdout;
 
-    if let Some(output) = args.output {
-        fs::write(&output, hex)
-            .with_context(|| anyhow!("Failed to write firmware to file {output:?}"))?;
-    } else {
-        let mut argv = Vec::new();
-        if let Some(timeout) = args.timeout {
-            argv.push(Cow::Borrowed("--timeout"));
-            argv.push(Cow::Owned(timeout.to_string()));
+            if let Some(output) = args.output {
+                fs::write(&output, hex)
+                    .with_context(|| anyhow!("Failed to write firmware to file {output:?}"))?;
+            } else {
+                let mut argv = Vec::new();
+                if let Some(timeout) = args.timeout {
+                    argv.push(Cow::Borrowed("--timeout"));
+                    argv.push(Cow::Owned(timeout.to_string()));
+                }
+                argv.extend([
+                    Cow::Borrowed("--run"),
+                    Cow::Borrowed("--no-ansi"),
+                    Cow::Borrowed("-"),
+                ]);
+                debug!("Starting micronucleus {args:?}");
+                let mut child = Command::new("micronucleus")
+                    .args(argv.iter().map(|x| x.as_ref()))
+                    .stdin(Stdio::piped())
+                    .spawn()
+                    .context("Failed to execute micronucleus")?;
+
+                {
+                    let mut stdin = child.stdin.take().unwrap();
+                    stdin.write_all(hex)?;
+                };
+
+                child.wait()?;
+            }
         }
-        argv.extend([
-            Cow::Borrowed("--run"),
-            Cow::Borrowed("--no-ansi"),
-            Cow::Borrowed("-"),
-        ]);
-        debug!("Starting micronucleus {args:?}");
-        let mut child = Command::new("micronucleus")
-            .args(argv.iter().map(|x| x.as_ref()))
-            .stdin(Stdio::piped())
-            .spawn()
-            .context("Failed to execute micronucleus")?;
-
-        {
-            let mut stdin = child.stdin.take().unwrap();
-            stdin.write_all(hex)?;
-        };
-
-        child.wait()?;
+        (None, Some(shell)) => {
+            args::gen_completions(shell)?;
+        }
+        _ => bail!("Usage: elf2nucleus <input>"),
     }
 
     Ok(())
